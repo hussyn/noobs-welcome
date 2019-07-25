766d414a60075b565335e8bbf3fd517243286e84
James Quick <james.q.quick@outlook.com>
2019-07-25 10:43:48 -0500

    Authentication is working and able to show/hide login/register buttons vs logout based on user being logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { FirebaseContext } from './Firebase';
-import { NavLink } from 'react-router-dom';
 export class Home extends Component {
     loggedIn = false;
 
@@ -8,7 +7,6 @@ export class Home extends Component {
         return (
             <FirebaseContext.Consumer>
                 {(firebase) => {
-                    console.log(firebase);
                     return <div>Welcome to Noobs Welcome!</div>;
                 }}
             </FirebaseContext.Consumer>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,30 +6,35 @@ export class Nav extends Component {
     render = () => {
         return (
             <UserContext.Consumer>
-                {(user) => (
-                    <nav>
-                        <div className="nav-brand">
-                            <NavLink to="/">
-                                <h1>Noobs Welcome</h1>
-                            </NavLink>
-                        </div>
-                        <div className="nav-content">
-                            {user && (
-                                <>
-                                    {/* <NavLink to="/add">Add Endpoint</NavLink> */}
-                                    <div className="divider" />
-                                    <NavLink to="/logout">logout</NavLink>
-                                </>
-                            )}
-                            {!user && (
-                                <>
-                                    <NavLink to="/login">Login</NavLink>
-                                    <NavLink to="/register">Register</NavLink>
-                                </>
-                            )}
-                        </div>
-                    </nav>
-                )}
+                {(user) => {
+                    console.log(user);
+                    return (
+                        <nav>
+                            <div className="nav-brand">
+                                <NavLink to="/">
+                                    <h1>Noobs Welcome</h1>
+                                </NavLink>
+                            </div>
+                            <div className="nav-content">
+                                {user && (
+                                    <>
+                                        {/* <NavLink to="/add">Add Endpoint</NavLink> */}
+                                        <div className="divider" />
+                                        <NavLink to="/logout">logout</NavLink>
+                                    </>
+                                )}
+                                {!user && (
+                                    <>
+                                        <NavLink to="/login">Login</NavLink>
+                                        <NavLink to="/register">
+                                            Register
+                                        </NavLink>
+                                    </>
+                                )}
+                            </div>
+                        </nav>
+                    );
+                }}
             </UserContext.Consumer>
         );
     };
